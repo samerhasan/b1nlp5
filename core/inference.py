5f61e9b0edd970d16073888b3664a272ede38874
Samer Hasan <samer.hasan85@gmail.com>
2018-12-08 19:54:32 +0530

    Update inference.py

--- core/inference.py
+++ core/inference.py
@@ -7,7 +7,7 @@
 import nltk
 from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
-from api.core import b1NER as bner
+from core import b1NER as bner
 import numpy as np
 import pickle
 import tflearn
